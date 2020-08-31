077a435b7623972316b3abc7c4eeb64afe58e4e7
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-08-30 21:34:12 -0500

    Added onClick to item in PrefabsScreen to go to the PrefabScreen. Populated the PrefabsScreen with one Prefab item.

--- lib/screens/prefabs_screen/prefabs_screen.dart
+++ lib/screens/prefabs_screen/prefabs_screen.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:prefab_app/contants.dart';
+import 'package:prefab_app/models/category.dart';
+import 'package:prefab_app/screens/prefab_screen/prefab_screen.dart';
 import 'package:prefab_app/widgets/custom_appbar.dart';
 import 'package:prefab_app/widgets/default_screen.dart';
+import 'package:prefab_app/widgets/image_text_tile.dart';
 
 class PrefabsScreen extends StatelessWidget {
   static final String routeName = '/prefabs';
+
+  final List<Category> categories = [
+    Category(name: 'Modular Prefab Housing Unit', imageUrl: kImages['prefab']),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return DefaultScreen(
       title: 'Prefabs',
       children: [
-        Container(
-          height: kSmallTileHeight,
-          width: kSmallTileHeight,
-          color: Colors.red,
-          // child: Column(children: [Image(image: Ass,)],),
+        Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          runSpacing: kDefaultMargin,
+          spacing: kDefaultMargin,
+          direction: Axis.horizontal,
+          children: categories
+              .map((cat) => ImageTextTile(
+                    width: kSmallTileHeight,
+                    height: kSmallTileHeight,
+                    name: cat.name,
+                    imageUrl: cat.imageUrl,
+                    onTap: () =>
+                        Navigator.pushNamed(context, PrefabScreen.routeName),
+                  ))
+              .toList(),
         )
       ],
     );
